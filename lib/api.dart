6bcd946deacc0361db67f330e63051fd1ea7cd62
rafaelbanhos <rafaelbanhos@gmail.com>
2020-04-24 13:36:01 +0100

    youtube app

--- lib/api.dart
+++ lib/api.dart
@@ -1,1 +1,32 @@
-const API_KEY = "AIzaSyB9isoBUs_6Y1mMAumHGeEsZVwsLPATWMw";
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:youtubeapp/models/video.dart';
+
+const API_KEY = "YOUR_KEY";
+
+class Api {
+  search(String search) async {
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10");
+
+    decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+
+      List<Video> videos = decoded["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      return videos;
+    } else {
+
+      throw Exception("Failed to load videos");
+
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:youtubeapp/api.dart';
 import 'package:youtubeapp/screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  
+  Api api = Api();
+  api.search("eletro");
+
+
+  runApp(MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
   @override
