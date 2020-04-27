37959f04b9625a8595bde60ce1e2a43142434aad
rafaelbanhos <rafaelbanhos@gmail.com>
2020-04-27 12:30:28 +0100

    creating favorite bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,29 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:youtubeapp/api.dart';
+import 'package:youtubeapp/blocs/favorite_bloc.dart';
 import 'package:youtubeapp/blocs/videos_bloc.dart';
 import 'package:youtubeapp/screens/home_screen.dart';
 
 void main() {
-  
   Api api = Api();
   api.search("eletro");
 
-
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      bloc: VideosBloc(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "YouTube App Flutter",
-        home: HomeScreen(),
-      ),
-    );
+        bloc: VideosBloc(),
+        child: BlocProvider(
+          bloc: FavoriteBloc(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "YouTube App Flutter",
+            home: HomeScreen(),
+          ),
+        ));
   }
 }
--- lib/widgets/video_tile.dart
+++ lib/widgets/video_tile.dart
@@ -1,22 +1,29 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:youtubeapp/blocs/favorite_bloc.dart';
 import 'package:youtubeapp/models/video.dart';
 
 class VideoTile extends StatelessWidget {
-
   final Video video;
 
   VideoTile(this.video);
 
   @override
   Widget build(BuildContext context) {
+
+    final favBloc = BlocProvider.of<FavoriteBloc>(context);
+
     return Container(
       margin: EdgeInsets.symmetric(vertical: 4),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           AspectRatio(
-            aspectRatio: 16.0/9.0,
-            child: Image.network(video.thumb, fit: BoxFit.cover,),
+            aspectRatio: 16.0 / 9.0,
+            child: Image.network(
+              video.thumb,
+              fit: BoxFit.cover,
+            ),
           ),
           Row(
             children: <Widget>[
@@ -26,30 +33,39 @@ class VideoTile extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                      child: Text(video.title,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16
-                      ),
+                      child: Text(
+                        video.title,
+                        style: TextStyle(color: Colors.white, fontSize: 16),
                         maxLines: 2,
                       ),
                     ),
                     Padding(
                       padding: EdgeInsets.all(8),
-                      child: Text(video.channel,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14
-                        ),),
+                      child: Text(
+                        video.channel,
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
                     )
                   ],
                 ),
               ),
-              IconButton(
-                icon: Icon(Icons.star_border),
-                color: Colors.white,
-                iconSize: 30,
-                onPressed: () {},
+              StreamBuilder<Map<String, Video>>(
+                stream: favBloc.outFav,
+                initialData: {},
+                builder: (context, snapshot) {
+                  if (snapshot.hasData)
+                    return IconButton(
+                      icon: Icon(snapshot.data.containsKey(video.id) ?
+                      Icons.star : Icons.star_border),
+                      color: Colors.white,
+                      iconSize: 30,
+                      onPressed: () {
+                        favBloc.toggleFavorite(video);
+                      },
+                    );
+                  else
+                    return CircularProgressIndicator();
+                },
               )
             ],
           )
