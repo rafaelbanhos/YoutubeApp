47f0fe6d5ebab660fc077e6640ed61cf5b4785fb
rafaelbanhos <rafaelbanhos@gmail.com>
2020-04-27 10:54:07 +0100

    configuring bloc

---
+++ lib/blocs/videos_bloc.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:youtubeapp/api.dart';
+import 'package:youtubeapp/models/video.dart';
+import 'dart:async';
+
+class VideosBloc extends BlocBase {
+
+  Api api;
+
+  List<Video> videos;
+
+  final StreamController _videosController = StreamController();
+  Stream get outVideos => _videosController.stream;
+
+  final StreamController _searchController = StreamController();
+  Sink get inSearch => _searchController.sink;
+
+  VideosBloc() {
+    api = Api();
+
+    _searchController.stream.listen(_search);
+  }
+
+  void _search(String search) async {
+
+    videos = await api.search(search);
+    print(videos);
+  }
+
+  @override
+  void dispose() {
+    _videosController.close();
+    _searchController.close();
+  }
+}
